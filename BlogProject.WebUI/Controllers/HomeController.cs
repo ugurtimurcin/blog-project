b1b303ad1f706f017029a5dcf9431e45d8cbe575
Ugur Timurcin <utimurcin@outlook.com>
2021-02-05 15:13:48 +0300

    Index and Detail action added to HomeController(main)

--- BlogProject.WebUI/Controllers/HomeController.cs
+++ BlogProject.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogProject.Business.Abstract;
+using BlogProject.DTO.Concrete.ArticleDTOs;
+using BlogProject.Entities.Concrete;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,21 @@ namespace BlogProject.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IGenericService<Article> _genericService;
+        public HomeController(IGenericService<Article> genericService)
         {
-            return View();
+            _genericService = genericService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var articles = await _genericService.GetAllAsync(x => x.Id);
+            return View(articles.Adapt<List<ArticleListDto>>());
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var article = await _genericService.GetByIdAsync(id);
+            return View(article.Adapt<ArticleListDto>());
         }
     }
 }
