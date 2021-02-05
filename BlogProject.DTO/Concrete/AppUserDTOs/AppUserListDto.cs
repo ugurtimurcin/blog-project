a3c8b548a3971d647e17bde52332c2bbece709e4
Ugur Timurcin <utimurcin@outlook.com>
2021-02-05 16:53:41 +0300

    AppUserListDto and AppUserEditDto added to DTO layer

---
+++ BlogProject.DTO/Concrete/AppUserDTOs/AppUserListDto.cs
@@ -0,0 +1,19 @@
+﻿using BlogProject.DTO.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.DTO.Concrete.AppUserDTOs
+{
+    public class AppUserListDto : IDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string ProfilPicture { get; set; }
+    }
+}
