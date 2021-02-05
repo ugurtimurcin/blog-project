e37c70d80845c52f17ce69ccaadfe8c0ce5d0db4
Ugur Timurcin <utimurcin@outlook.com>
2021-02-05 17:11:43 +0300

    Password changing dto added

---
+++ BlogProject.DTO/Concrete/AppUserDTOs/AppUserPasswordChangeDto.cs
@@ -0,0 +1,16 @@
+﻿using BlogProject.DTO.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.DTO.Concrete.AppUserDTOs
+{
+    public class AppUserPasswordChangeDto : IDto
+    {
+        public string Email { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
