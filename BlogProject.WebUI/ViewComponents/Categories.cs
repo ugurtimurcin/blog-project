7f06f2a2d2c14bb851d81c20f76fa0c0387ed8f8
Ugur Timurcin <utimurcin@outlook.com>
2021-02-05 15:14:22 +0300

    Component created

---
+++ BlogProject.WebUI/ViewComponents/Categories.cs
@@ -0,0 +1,26 @@
+﻿using BlogProject.Business.Abstract;
+using BlogProject.DTO.Concrete.CategoryDTOs;
+using BlogProject.Entities.Concrete;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.WebUI.ViewComponents
+{
+    public class Categories : ViewComponent
+    {
+        private readonly IGenericService<Category> _genericService;
+        public Categories(IGenericService<Category> genericService)
+        {
+            _genericService = genericService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categories = _genericService.GetAllAsync(x => x.Id).Result;
+            return View(categories.Adapt<List<CategoryListDto>>());
+        }
+    }
+}
