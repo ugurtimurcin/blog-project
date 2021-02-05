2d513af3c65c3e09b37f51e0b8c3ebd71f8dca67
Ugur Timurcin <utimurcin@outlook.com>
2021-02-05 17:12:35 +0300

    actions added to profileController

--- BlogProject.WebUI/Areas/Admin/Controllers/ProfileController.cs
+++ BlogProject.WebUI/Areas/Admin/Controllers/ProfileController.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BlogProject.DTO.Concrete.AppUserDTOs;
+using BlogProject.Entities.Concrete;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +17,79 @@ namespace BlogProject.WebUI.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        public ProfileController(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            TempData["Active"] = "profile";
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(user.Adapt<AppUserListDto>());
+        }
+
+        public async Task<IActionResult> Edit()
+        {
+            TempData["Active"] = "profile";
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(user.Adapt<AppUserEditDto>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppUserEditDto appUser, IFormFile pic)
+        {
+            var user = await _userManager.FindByIdAsync(appUser.Id);
+            if (pic != null)
+            {
+                var picName = user.UserName + "_" + Guid.NewGuid() + Path.GetExtension(pic.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/profile-picture/" + picName);
+                using var stream = new FileStream(path, FileMode.Create);
+                await pic.CopyToAsync(stream);
+
+                user.ProfilPicture = picName;
+            }
+
+            user.FirstName = appUser.FirstName;
+            user.LastName = appUser.LastName;
+            user.Email = appUser.Email;
+            user.ProfilPicture = user.ProfilPicture;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                TempData["Success"] = "Your profile has updated!";
+                return RedirectToAction("Index", "Profile", new { area = "Admin" });
+            }
+
+            return View(appUser);
+        }
+
+        public IActionResult Password()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Password(AppUserPasswordChangeDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        TempData["Success"] = "Your password has changed!";
+                        return RedirectToAction("Index", "Profile", new { area = "Admin" });
+                    }
+                }
+            }
             return View();
         }
     }
