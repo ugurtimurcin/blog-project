29a0da1840cd4a369fa5b75073d42593966eeafa
Ugur Timurcin <utimurcin@outlook.com>
2021-02-07 13:41:39 +0300

    List and edit dto created for comment

---
+++ BlogProject.DTO/Concrete/CommentDTOs/CommentListDto.cs
@@ -0,0 +1,22 @@
+﻿using BlogProject.Core.Entities;
+using BlogProject.DTO.Concrete.ArticleDTOs;
+using BlogProject.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.DTO.Concrete.CommentDTOs
+{
+    public class CommentListDto : IDto
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Content { get; set; }
+        public bool IsActive { get; set; }
+
+        public ArticleListDto Article { get; set; }
+    }
+}
